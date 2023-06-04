cb975a77cb6fa87c5f382656e28b29d756965f3f
l646505418 <646505418@qq.com>
2023-06-04 11:05:33 +0800

    complete lab1

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -24,13 +24,63 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type WorkerRequest struct {
+	WorkerId string
+	WorkDone int
+}
+
+//type WorkerResponse struct {
+//	FileInfo    string
+//	Type        ResponseType
+//	NumReduce   int
+//	JobId       int
+//	TmpFileList []string
+//}
+
+////condition of response
+//type ResponseType int
+//
+//const (
+//	Map ResponseType = iota
+//	Reduce
+//	Wait
+//	KillYourself
+//)
+
+//condition of job
+type JobCondition int
+
+const (
+	JobWorking = iota
+	JobWaiting
+	JobDone
+)
+
+//type of job
+type JobType int
+
+const (
+	MapJob = iota
+	ReduceJob
+	WaittingJob
+	KillJob
+)
+
+//condition of coordinator
+type Condition int
+
+const (
+	MapPhase = iota
+	ReducePhase
+	AllDone
+)
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
 // Can't use the current directory since
 // Athena AFS doesn't support UNIX-domain sockets.
 func coordinatorSock() string {
-	s := "/var/tmp/5840-mr-"
+	s := "/var/tmp/824-mr-"
 	s += strconv.Itoa(os.Getuid())
 	return s
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,18 +1,30 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
+// KeyValue
 // Map functions return a slice of KeyValue.
 //
 type KeyValue struct {
 	Key   string
 	Value string
 }
+type ByKey []KeyValue
+
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -24,21 +36,53 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
+// Worker
 // main/mrworker.go calls this function.
 //
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string, id string) {
+	workerId := id
+	alive := true
+	attempt := 0
 
-	// Your worker implementation here.
+	for alive {
+		attempt++
+		fmt.Println(workerId, " >worker ask", attempt)
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+		job := RequireTask(workerId)
+		fmt.Println(workerId, "worker get job", job)
+		switch job.JobType {
+		case MapJob:
+			{
+				DoMap(mapf, job)
+				fmt.Println("do map", job.JobId)
+				JobIsDone(workerId, job)
+			}
+		case ReduceJob:
+			if job.JobId >= 8 {
+				DoReduce(reducef, job)
+				fmt.Println("do reduce", job.JobId)
+				JobIsDone("kaka", job)
+			}
+
+		case WaittingJob:
+			fmt.Println("get waiting")
+			time.Sleep(time.Second)
+		case KillJob:
+			{
+				time.Sleep(time.Second)
+				fmt.Println("[Status] :", workerId, "terminated........")
+				alive = false
+			}
+		}
+		time.Sleep(time.Second)
+
+	}
+
+	//CallForMap(workerId)
 
 }
 
-//
+// CallExample
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
@@ -55,27 +99,34 @@ func CallExample() {
 	reply := ExampleReply{}
 
 	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
-	}
+	call("Coordinator.Example", &args, &reply)
+
+	// reply.Y should be 100.
+	fmt.Printf("reply.Y %v\n", reply.Y)
+}
+
+func RequireTask(workerId string) *Job {
+	args := ExampleArgs{}
+
+	reply := Job{}
+
+	call("Coordinator.DistributeJob", &args, &reply)
+	fmt.Println("get response", &reply)
+
+	return &reply
+
 }
 
 //
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
+
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := coordinatorSock()
-	c, err := rpc.DialHTTP("unix", sockname)
+	//sockname := coordinatorSock()
+	//c, err := rpc.DialHTTP("unix", sockname)
+	c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
@@ -88,4 +139,90 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+func JobIsDone(workerId string, job *Job) {
+	args := job
+	reply := &ExampleReply{}
+	call("Coordinator.JobIsDone", &args, &reply)
+}
+
+func DoMap(mapf func(string, string) []KeyValue, response *Job) {
+	var intermediate []KeyValue
+	filename := response.InputFile[0]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	intermediate = mapf(filename, string(content))
+
+	//initialize and loop over []KeyValue
+	rn := response.ReducerNum
+	HashedKV := make([][]KeyValue, rn)
+
+	for _, kv := range intermediate {
+		HashedKV[ihash(kv.Key)%rn] = append(HashedKV[ihash(kv.Key)%rn], kv)
+	}
+	for i := 0; i < rn; i++ {
+		oname := "mr-tmp-" + strconv.Itoa(response.JobId) + "-" + strconv.Itoa(i)
+		ofile, _ := os.Create(oname)
+		enc := json.NewEncoder(ofile)
+		for _, kv := range HashedKV[i] {
+			enc.Encode(kv)
+		}
+		ofile.Close()
+	}
+
+}
+
+func DoReduce(reducef func(string, []string) string, response *Job) {
+	reduceFileNum := response.JobId
+	intermediate := readFromLocalFile(response.InputFile)
+	sort.Sort(ByKey(intermediate))
+	dir, _ := os.Getwd()
+	//tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("Failed to create temp file", err)
+	}
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	tempFile.Close()
+	oname := fmt.Sprintf("mr-out-%d", reduceFileNum)
+	os.Rename(tempFile.Name(), oname)
+}
+
+func readFromLocalFile(files []string) []KeyValue {
+	var kva []KeyValue
+	for _, filepath := range files {
+		file, _ := os.Open(filepath)
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+		file.Close()
+	}
+	return kva
 }
